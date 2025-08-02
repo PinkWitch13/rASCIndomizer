5530a42e178e558736774da9093208238265e8f1
PinkWitch13 <pinkwitch13@proton.me>
2025-08-02 17:04:06 +0200

    finished v2 bruteforce password cracker

--- passcracker/my_pcrack.py
+++ passcracker/my_pcrack.py
@@ -1,84 +1,61 @@
 import string
 import random
 
-
-
 def example_pass_breaker():
 
-    with open("Github/rASCIndomizer/passcracker/example_pass.txt", "r") as file:
+    with open("rASCIndomizer/passcracker/example_pass.txt", "r") as file:
         mpass = file.read()
     i = 1
     chars = ["a", "b", "c"]
     max_pass_len = 3
     max_combi = 3**3
     atempt = 0
+    combinations = []
 
     while i < max_combi:
         to_try = "".join(random.sample(chars, max_pass_len))
+        combinations.append(to_try)
+        print(f"{to_try=}")
+        print(f"{combinations=}")
         atempt+=1
         if to_try == mpass:
             print(f"Pasword broken! Your password is {mpass}, guest at {atempt=}")
             break
         else:
             i+=1
-            return (to_try, mpass)
     
-example_pass_breaker()
+# example_pass_breaker()
 
 
+def rASCIndomizer2():
 
+    with open("rASCIndomizer/passcracker/example_pass.txt", "r") as file:
+        mpass = file.read()
+    
+    chars = list(string.ascii_letters)+ list(string.digits)
+    max_pass_len = 4
+    i = 1
+    max_combi = len(chars)**max_pass_len
+    atempt = 0
+    combinations = []
+    stop = False
 
-
-
-# # import math
-
-# chars = ["a", "b", "c"]
-
-# chars = list(string.ascii_letters + string.digits)
-# cl = len(chars)
-# mcn = cl**cl
+    while i <= max_combi and not stop:
+        for n in range(1, max_pass_len+1):
+            n =4
+            #print(f"{n=} {chars=}")
+            to_try = "".join(random.sample(chars, n))
+            combinations.append(to_try)
+            #print(f"{to_try=}")
+            #print(f"{combinations=}")
+            atempt+=1
+            if to_try == mpass:
+                print(f"Pasword broken! Your password is {mpass}, guest at {atempt=}")
+                stop = True
+                break
+            else:
+                i+=1
+                continue
    
 
-# i = 1
-# atempt = 0
-# checked_combi = list()
-
-# with open("My_Projects/passcracker/example_pass.txt", "r") as file:
-#     mpass = file.read()
-
-# while i <= mcn:
-#     for p, char in enumerate(chars):
-#         to_try = "".join(random.sample(chars, i))
-#         checked_combi.append(to_try)
-#         if to_try in checked_combi:
-#             continue
-#         else:
-#             checked_combi.append(to_try)
-#             atempt+=1
-#             if to_try == mpass:
-#                 print(f"Password broken! Your password is {mpass}")
-#                 break
-#             else:
-#                 i+=1
-#                 print(to_try, f"{atempt=}")
-
-
-# max_pass_len = math.pow(char_len, char_len)
-
-# i = 1
-
-# for i in max_pass_len:
-#     posi_pass = set()
-
-
-# l = 1
-
-# while l <= max_pass_len:
-#     try_combi = set()
-#     for c in range(l):
-#         try_pass = str(random.shuffle(chars))
-#         try_combi.add(try_pass)
-#         print(try_combi)
-# print("CANNOT BREAK PASSWORD :(")
-
-chars = ["a", "b", "c"]
+rASCIndomizer2()
